675e1a9b5e3d391e9f1041f1f5d15f3eefba7379
dmitr1y <aser00707@ya.ru>
2015-05-09 00:55:20 +0300

    Konstantinov, Azarov, Fedorova
    + SUB_QQ_Q

--- DMColloquium/RationalNumber.cpp
+++ DMColloquium/RationalNumber.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "RationalNumber.h"
+
+RationalNumber RationalNumber::SUB_QQ_Q(RationalNumber rat_num_2)
+{
+	//
+	//  SUB_QQ_Q
+	//  DMColloquium
+	//
+	//  Created by Dmitry Konstantinov, Dmitry Azarov & Tuyaara Fedorova on 8/7/15.
+	//
+	RationalNumber result;
+	result.denominator=denominator.LCM_NN_N(rat_num_2.denominator);
+	this->numerator = this->numerator.MUL_ZZ_Z(this->numerator.TRANS_N_Z(result.denominator.DIV_NN_N(this->denominator)));
+	rat_num_2.numerator = rat_num_2.numerator.MUL_ZZ_Z(rat_num_2.numerator.TRANS_N_Z(result.denominator.DIV_NN_N(rat_num_2.denominator)));
+	result.numerator = this->numerator.SUB_ZZ_Z(rat_num_2.numerator);
+	return result;
+}

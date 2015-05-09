bcf7a411ae5ac1cd4a50e77f99b94cbada5fca2b
dmitr1y <aser00707@ya.ru>
2015-05-09 15:58:31 +0300

    Konstantinov, Azarov, Fedorova
    add err description to SUB_QQ_Q

--- DMColloquium/RationalNumber.cpp
+++ DMColloquium/RationalNumber.cpp
@@ -17,9 +17,16 @@ RationalNumber RationalNumber::SUB_QQ_Q(RationalNumber rat_num_2)
 	//  Created by Dmitry Konstantinov, Dmitry Azarov & Tuyaara Fedorova on 8/7/15.
 	//
 	RationalNumber result;
-	result.denominator=denominator.LCM_NN_N(rat_num_2.denominator);
-	this->numerator = this->numerator.MUL_ZZ_Z(this->numerator.TRANS_N_Z(result.denominator.DIV_NN_N(this->denominator)));
-	rat_num_2.numerator = rat_num_2.numerator.MUL_ZZ_Z(rat_num_2.numerator.TRANS_N_Z(result.denominator.DIV_NN_N(rat_num_2.denominator)));
-	result.numerator = this->numerator.SUB_ZZ_Z(rat_num_2.numerator);
+	if (!this->denominator.NZER_N_B() || !rat_num_2.denominator.NZER_N_B())
+	{
+		result.denominator = denominator.LCM_NN_N(rat_num_2.denominator);
+		this->numerator = this->numerator.MUL_ZZ_Z(this->numerator.TRANS_N_Z(result.denominator.DIV_NN_N(this->denominator)));
+		rat_num_2.numerator = rat_num_2.numerator.MUL_ZZ_Z(rat_num_2.numerator.TRANS_N_Z(result.denominator.DIV_NN_N(rat_num_2.denominator)));
+		result.numerator = this->numerator.SUB_ZZ_Z(rat_num_2.numerator);
+	}
+	else
+	{
+		//ERR: denominator = 0
+	}
 	return result;
 }

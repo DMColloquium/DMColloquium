26cde2c481b082b50e9f9ee5d4ff6235bd7cd007
VitaliyGit <lazeev.vo63@gmail.com>
2015-05-11 02:26:56 +0400

    Update NaturalNumber.cpp

--- DMColloquium/NaturalNumber.cpp
+++ DMColloquium/NaturalNumber.cpp
@@ -1,18 +1,24 @@
-//
-//  NaturalNumber.cpp
-//  DMColloquium
-//
-//  Created by Ilya Kulebyakin on 4/28/15.
-//  Copyright (c) 2015 Ilya Kulebyakin. All rights reserved.
-//
-
 #include "NaturalNumber.h"
-using namespace std;
-
-// put definitions for all methods over here.
 
 NaturalNumber::NaturalNumber() {
-    digitBlocks = std::vector<unsigned int>(1);
-    digitBlocks[0] = 0;
+	digitBlocks = std::vector <unsigned int>(1);
+	digitBlocks[0] = 0;
 }
 
+int NaturalNumber::COM_NN_D(NaturalNumber n) {
+	// this function compares two natural numbers:
+	// if n1 = n2, function returns 0;
+	// if n1 > n2, function returns 2;
+	// else function returns 1
+	if (this->digitBlocks == n.digitBlocks) return 0;
+	if (this->digitBlocks.size() > n.digitBlocks.size()) return 2; 
+	if (this->digitBlocks.size() == n.digitBlocks.size()) {
+		for (int j = this->digitBlocks.size() - 1;; j > -1; --j) {
+			if (this->digitBlocks[j] > n.digitBlocks[j])
+				return 2;
+			else if (this->digitBlocks[j] < n.digitBlocks[j])
+				return 1;
+		}
+	}
+	return 1; 
+}

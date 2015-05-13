9748f17e5ff5409bc760d7e4efd6cfd29becdb72
galandezzz <fearum@icloud.com>
2015-05-13 22:42:25 +0300

    Added DIV_QQ_Q

--- DMColloquium/RationalNumber.h
+++ DMColloquium/RationalNumber.h
@@ -1,28 +1,17 @@
-//
-//  RationalNumber.h
-//  DMColloquium
-//
-//  Created by Ilya Kulebyakin on 4/28/15.
-//  Copyright (c) 2015 Ilya Kulebyakin. All rights reserved.
-//
-
 #ifndef __DMColloquium__RationalNumber__
 #define __DMColloquium__RationalNumber__
 
 #include "Integer.h"
 #include "NaturalNumber.h"
 
-
-
 class RationalNumber {
 private:
     Integer numerator;
     NaturalNumber denominator;
 public:
-    // Instanciates a rational number (0/1)
     RationalNumber();
     RationalNumber(Integer numerator, NaturalNumber denominator):numerator{numerator},denominator{denominator}{};
-    // declarations for methods
+    RationalNumber DIV_QQ_Q(RationalNumber);
 };
 
-#endif
+#endif /*defined(__DMColloquium__RationalNumber__)*/

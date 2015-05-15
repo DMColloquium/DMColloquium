57b3d767d29de3ac4d51128b19f936c77670cb57
NetZloj <naumenko.nikita@mail.ru>
2015-05-15 19:29:10 +0300

    Update RationalNumber.cpp

--- DMColloquium/RationalNumber.cpp
+++ DMColloquium/RationalNumber.cpp
@@ -7,3 +7,20 @@
 //
 
 #include "RationalNumber.h"
+
+RationalNumber::RationalNumber() {
+    numerator = Integer();
+    denominator = NaturalNumber();
+    //denominator.ADD_1N_N();
+}
+
+//Q-7
+//Умножение дробей
+RationalNumber RationalNumber::MUL_QQ_Q(RationalNumber n)
+{
+	RationalNumber n3;
+	n3.numerator = numerator.MUL_ZZ_Z(n.numerator);//Умножение целых чисел
+	n3.denominator = denominator.MUL_NN_N(n.denominator);//умножение натуральных чисел
+	n3 = RED_Q_Q(n3);//сокращение дроби
+	return (n3);
+}

acbb090c915831f077851646e54655491a452f03
NetZloj <naumenko.nikita@mail.ru>
2015-05-15 19:09:35 +0300

    Update Integer.cpp

--- DMColloquium/Integer.cpp
+++ DMColloquium/Integer.cpp
@@ -7,3 +7,23 @@
 //
 
 #include "Integer.h"
+#include "stdafx.h"
+using namespace std;
+
+Integer::Integer() {
+    digitBlocks = vector<unsigned int>(1);
+    digitBlocks[0] = 0;
+    isPositive = true;
+}
+
+Integer::Integer(vector<unsigned int> digits, bool isPositive) {
+    digitBlocks = digits;
+    this->isPositive = isPositive;
+}
+
+//Z-9 Науменко и Смагин
+//Остаток от деления большего целого числа на меньшее или равное натуральное
+Integer MOD_ZZ_Z(Integer a, NaturalNumber b)
+{
+	return SUB_ZZ_Z(a, MUL_ZZ_Z(TRANS_N_Z(b), DIV_ZZ_Z(a, b)));
+}

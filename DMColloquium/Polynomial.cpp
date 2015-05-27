33c401fda7b9e40189af3b5c732a2bd60b86ba01
Ceyler997 <Ceyler997@users.noreply.github.com>
2015-05-27 21:14:29 +0300

    Update Polynomial.cpp

--- DMColloquium/Polynomial.cpp
+++ DMColloquium/Polynomial.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "Polynomial.h"
+Polynomial Polynomial::ADD_PP_P(Polynomial p2)
+{
+	int i;
+	if (this->coefficients.size() > p2.coefficients.size())
+	{
+		for (i = 0; i < p2.coefficients.size(); i++)
+			this->coefficients[i] = this->coefficients[i].ADD_QQ_Q(p2.coefficients[i]);
+		return *this;
+	}
+	else
+	{
+		for (i = 0; i < this->coefficients.size(); i++)
+			p2.coefficients[i] =p2.coefficients[i].ADD_QQ_Q(p2.coefficients[i]);
+		return p2;
+	}
+}

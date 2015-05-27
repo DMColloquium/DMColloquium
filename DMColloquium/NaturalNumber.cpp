6cfc19ea55c2e4628875da295804f325aaea9053
Ceyler997 <Ceyler997@users.noreply.github.com>
2015-05-27 21:11:00 +0300

    Update NaturalNumber.cpp

--- DMColloquium/NaturalNumber.cpp
+++ DMColloquium/NaturalNumber.cpp
@@ -14,3 +14,25 @@
 NaturalNumber NaturalNumber::greatestCommonFactor(NaturalNumber secondNumber){
     return *this;
 }
+
+NaturalNumber NaturalNumber::SUB_NN_N(NaturalNumber nat_num2)
+{
+	int i;
+	if (this->COM_NN_D(nat_num2)==2)
+	{
+		for (i = 0; i < this->digitBlocks.size(); i++)
+		{
+			while (this->digitBlocks[i]<nat_num2.digitBlocks[i])
+			{
+				this->digitBlocks[i + 1]--;
+				this->digitBlocks[i] += 1000000000;
+			};
+			this->digitBlocks[i] -= nat_num2.digitBlocks[i];
+		}
+	}
+	else
+	{
+		cout << "Error!";
+	}
+	return *this;
+}
